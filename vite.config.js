a84793ffe8e7e219f4c3060048bef83331b08f84
Sh00bham <shubhamgowai2020@gmail.com>
2024-08-08 00:08:13 +0530

    first deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import { VitePluginGHPages } from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePluginGHPages()],
+  base: 'https://shekhar8689.github.io/fitpeo', 
+});
